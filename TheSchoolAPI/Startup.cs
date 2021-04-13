4b583a4923f743ba8ee2c608040202bad7aaeee8
LenTheDev <lhl7@njit.edu>
2021-04-13 08:33:28 -0400

    "Task: Complete Module 5"

--- TheSchoolAPI/Startup.cs
+++ TheSchoolAPI/Startup.cs
@@ -1,22 +1,20 @@
+using SchoolAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using NLog;
+using System.IO;
 
-namespace TheSchoolAPI
+namespace SchoolAPI
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
             Configuration = configuration;
         }
 
@@ -25,6 +23,8 @@ namespace TheSchoolAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureLoggerService();
+
             services.AddControllers();
         }
 
@@ -35,8 +35,6 @@ namespace TheSchoolAPI
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
