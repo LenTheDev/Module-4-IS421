f3bcb8edf266684435f066cb7653bfd342d0ab5d
LenTheDev <lhl7@njit.edu>
2021-04-16 05:38:56 -0400

    Task: Module 5

--- TheSchoolAPI/Controllers/WeatherForecastController.cs
+++ TheSchoolAPI/Controllers/WeatherForecastController.cs
@@ -1,24 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TheSchoolAPI;
 
-namespace TheSchoolAPI.Controllers
+namespace SchoolAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private ILoggerManager _logger;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILoggerManager logger)
         {
             _logger = logger;
         }
@@ -27,6 +29,10 @@ namespace TheSchoolAPI.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+            _logger.LogInfo("Info message from our controller.");
+            _logger.LogDebug("Debug message from our controller.");
+            _logger.LogWarn("Warn message from our controller.");
+            _logger.LogError("Error message from our controller.");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
